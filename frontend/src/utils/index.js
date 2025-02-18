530aff27ffa5c5cdcd69da54c65527c183a3a0c9
pauly7610 <161557030+pauly7610@users.noreply.github.com>
2025-02-18 02:18:57 -0500

    new index.js

---
+++ frontend/src/utils/index.js
@@ -0,0 +1,12 @@
+// Centralized exports for utility functions and classes
+
+export { 
+  ImageCache, 
+  PersistentImageCache, 
+  getPropertyStockImages, 
+  getPropertyTypeImage, 
+  scoreImageRelevance,
+  ImageFetchError 
+} from './stockImages';
+
+// You can add more utility exports here in the future 
